cd24289532d2922bca40bffb1e80d3f66ecfb260
vlion <vlionix@gmail.com>
2011-03-05 12:14:12 -0800

    more or less ready state

--- emit-html.pl
+++ emit-html.pl
@@ -1,15 +1,23 @@
-open( my $fh, "<", "quicklisp_documentation.txt") or die($!);
+use strict;
+use warnings;
+
+my $fh;
+
+open( $fh, "<", "quicklisp_documentation.txt") or die($!);
 my @arr = <$fh>;
 close $fh;
 
 open( $fh, ">", "quicklisp_documentation.html") or die($!);
 # Add in some better html conformance
-print $fh "<table width=1>";
+print $fh "<html>";
+print $fh "<body>";
+print $fh "<table border=1>";
 foreach my $line (@arr)
 {
     my ($name, $packages, $desc) = split(/%%/, $line);
     print $fh "<tr><td> $name </td><td> $desc </td></tr>\n";
 }
 print $fh "</table>\n";
+print $fh "<body>";
 close $fh;
 
---
+++ readme.txt
@@ -0,0 +1,9 @@
+requirements:
+ Perl 5.8 or so.
+
+instructions:
+cd to the directory of operation and run:
+       
+  make
+
+builds a html file that describes the different Quicklisp systems
