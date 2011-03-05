531c6c7d75a31744460e260215924360565cef0e
vlion <vlionix@gmail.com>
2011-03-05 13:53:26 -0800

    Fixed the perl to handle multiple lines in a field. also made more general. Updated makefile

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all: quicklisp_documentation.html
 
 
 quicklisp_documentation.html: quicklisp_documentation.txt
-	perl emit-html.pl
+	perl emit-html.pl quicklisp_documentation.txt quicklisp_documentation.html
 
 clean:
 	rm quicklisp_documentation.html
--- emit-html.pl
+++ emit-html.pl
@@ -1,20 +1,45 @@
 use strict;
 use warnings;
 
+sub trim 
+{
+    my $str = shift;
+    $str =~ s/^\s+(.*)\s+$/$1/;
+}
+
+my ($input, $output) = @ARGV;
+
+die("Syntax: perl emit-html.pl $0 infile outfile") unless $input && $output;
+
 my $fh;
 
-open( $fh, "<", "quicklisp_documentation.txt") or die($!);
-my @arr = <$fh>;
-close $fh;
+my $data;
+{ 
+    local $/ = undef;
+    open( $fh, "<", $input) or die($!);
+    $data = <$fh>;
+    close $fh;
+}
+# data has the form name %% systems %%  desc %%
+#pulls the data in using parallel arrays
+my @entries = $data =~ m/(.+?)%%
+    (.+?)%%
+    (.+?)%%/msgx;
 
-open( $fh, ">", "quicklisp_documentation.html") or die($!);
+#reshape the arrays to be less parallel.
+@entries = map { 
+    [ $entries[$_], 
+      $entries[$_+1], 
+      $entries[$_+2]] }  (map { $_ * 3 } (0 .. $#entries / 3));
+
+open( $fh, ">", $output) or die($!);
 # Add in some better html conformance
 print $fh "<html>";
 print $fh "<body>";
 print $fh "<table border=1>";
-foreach my $line (@arr)
+foreach my $data_ref (@entries)
 {
-    my ($name, $packages, $desc) = split(/%%/, $line);
+    my ($name, $packages, $desc) = @$data_ref;
     print $fh "<tr><td> $name </td><td> $desc </td></tr>\n";
 }
 print $fh "</table>\n";
